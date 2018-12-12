effdc4cfd4dfdfd49472cc0f38a4f9a7e54bfd8c
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2018-12-13 01:20:28 +0200

    Create postgres database

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
+import psycopg2
+
 from flask import Flask
+
 app = Flask(__name__)
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route("/")
 def hello():
