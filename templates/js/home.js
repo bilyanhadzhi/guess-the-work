676bc7e0f41445eb160fd9aa9e99e31cc0bfe8f8
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2018-12-24 00:03:39 +0200

    Start working on front-end logic
    Make footer sit always on the bottom

--- templates/js/home.js
+++ templates/js/home.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('loaded');
+    var game = {
+        init: function() {
+            this.populateAuthors();
+        },
+        populateAuthors: function() {
+            var authors = {{ authors|safe }};
+            this.authors = {};
+
+            for (var i = 0; i < authors.length; ++i) {
+                this.authors[i] = {};
+                this.authors[i].name = authors[i];
+                this.authors[i].picked = true;
+            }
+        },
+        cacheDOM: function() {
+
+        },
+        bindEvents: function() {
+
+        },
+    };
+
+    game.init();
+    console.log(game.authors);
 });
 
