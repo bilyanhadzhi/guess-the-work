c42258be51dddb94a25b3fe4afcb0830c6921a15
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2019-01-07 22:34:20 +0200

    Make /get_excerpts API point acceptable

--- app.py
+++ app.py
@@ -22,21 +22,41 @@ def home():
 def api_get_excerpt():
     author_ids = request.args.get('author_ids')
 
-    if (author_ids is None):
-        return 'No author IDs supplied', 404
-
     author_ids = author_ids.split(',')
-    author_ids = [author_id for author_id in author_ids if author_id.isnumeric()]
+    author_ids = [author_id for author_id in author_ids if author_id.isnumeric() and 1 <= int(author_id) <= 15]
+
+    if not author_ids:
+        return ''
+
     random_id = random.choice(author_ids)
+
+    print('-----------')
+    print('author id:', random_id)
+
 
     cur = conn.cursor()
     cur.execute("SELECT content FROM works \
+                WHERE author_id = {0} \
                 ORDER BY random() \
-                LIMIT 1")
+                LIMIT 1".format(random_id))
 
-    result = cur.fetchone()
+    result = ''.join(cur.fetchone())
+    result = result.split('\n')
 
-    print(result)
-    print(type(result))
+    # print(result)
+    # print(type(result))
 
-    return 'ok'
+    if (len(result) > 10):
+        # grab 10 lines
+        begin_index = random.randint(0, len(result) - 11)
+        end_index = begin_index + 10
+
+        print('begin: ', begin_index)
+        print('end: ', end_index)
+        result = '\n'.join(result[begin_index:begin_index + 10])
+    else:
+        return ''
+
+    print('len: ', len(result))
+
+    return result
